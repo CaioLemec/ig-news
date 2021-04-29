e2319717e27f0bada49caa673b235fe0b4e07b4e
Caio Lemec <caio.lemec@gmail.com>
2021-04-29 20:20:00 -0300

    using asPath to style buttons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,21 @@
 import { SignInButton } from '../SignInButton'
 import Link from 'next/link';
 import styles from './styles.module.scss'
+import { useRouter } from 'next/dist/client/router';
 
 export function Header() {
+    const { asPath } = useRouter();
+
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news"/>
                 <nav>
                     <Link href="/">
-                        <a className={styles.active}>Home</a>
+                        <a className={asPath === '/' ? styles.active : ''}>Home</a>
                     </Link>
                     <Link href="/posts">
-                        <a>Posts</a>
+                        <a className={asPath === '/posts' ? styles.active : ''}>Posts</a>
                     </Link>
                 </nav>
                 <SignInButton />
